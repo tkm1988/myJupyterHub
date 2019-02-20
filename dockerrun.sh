f6f029aae9229e2c60bb4ec98139f259ce354743
tkm1988 <bumpbumpbump.v@gmail.com>
2019-02-20 17:57:52 +0900

    Can use the JupyterLab

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker container run --rm -p 8080:8000 -h "dtkmjh1f" --name "dtkmjh1f" -d \
+       -v /Users/tkm/GitHub/myJupyterHub/src:/home/tkm1988/notebooks/ \
        myjupyterhub/myjupyterhub:latest \
        jupyterhub --no-ssl --config=/opt/jupyterhub_config.py
